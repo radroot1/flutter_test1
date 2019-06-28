c61dd1349c0acb598ff2622be3dc79b6f72f908e
radroot <radroot@gmail.com>
2019-06-28 14:57:59 +0400

    #00 part 6 ref+

--- lib/tabs/animations/02logo.dart
+++ lib/tabs/animations/02logo.dart
@@ -1,37 +1,10 @@
-// #docregion ShakeCurve
-import 'dart:math';
-
-// #enddocregion ShakeCurve
 import 'package:flutter/animation.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(LogoApp());
+
 class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
-}
-
-// #docregion diff
-class AnimatedLogo extends AnimatedWidget {
-  // Make the Tweens static because they don't change.
-  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
-  static final _sizeTween = Tween<double>(begin: 0, end: 300);
-
-  AnimatedLogo({Key key, Animation<double> animation})
-      : super(key: key, listenable: animation);
-
-  Widget build(BuildContext context) {
-    final Animation<double> animation = listenable;
-    return Center(
-      child: Opacity(
-        opacity: _opacityTween.evaluate(animation),
-        child: Container(
-          margin: EdgeInsets.symmetric(vertical: 10),
-          height: _sizeTween.evaluate(animation),
-          width: _sizeTween.evaluate(animation),
-          child: FlutterLogo(),
-        ),
-      ),
-    );
-  }
 }
 
 class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
@@ -41,34 +14,36 @@ class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    // #docregion AnimationController, tweens
     controller =
         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-    // #enddocregion AnimationController, tweens
-    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed) {
-          controller.reverse();
-        } else if (status == AnimationStatus.dismissed) {
-          controller.forward();
-        }
+    // #docregion addListener
+    animation = Tween<double>(begin: 0, end: 300).animate(controller)
+      ..addListener(() {
+        // #enddocregion addListener
+        setState(() {
+          // The state that has changed here is the animation object’s value.
+        });
+        // #docregion addListener
       });
+    // #enddocregion addListener
     controller.forward();
   }
 
   @override
-  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      ),
+    );
+  }
 
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
-}
-// #enddocregion diff
-
-// #docregion ShakeCurve
-class ShakeCurve extends Curve {
-  @override
-  double transform(double t) => sin(t * pi * 2);
 }
